154894aa3d67aa38a477e97550af616bccf12a70
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-06-12 14:17:32 +0200

    Added skeleton of comparison bar char

--- chart.js
+++ chart.js
@@ -1,4 +1,6 @@
 let selectedItems = [];
+var langueageIdFirst = 0;
+var langueageIdSecond = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
     drawBubbleChart(generalLanguages);
@@ -50,6 +52,7 @@ let drawBubbleChart = (dataset) => {
         .attr("width", width)
         .attr("height", height)
 
+
     // color palette
     var color = d3.scaleOrdinal()
         .domain(["Procedural", "Script", "Object_oriented", "Functional", "Declarative"])
@@ -98,6 +101,11 @@ let drawBubbleChart = (dataset) => {
         }
 
         console.log(selectedItems);
+        var langueageIdFirst = selectedItems[0];
+        var langueageIdSecond = selectedItems[1];
+
+        console.log(langueageIdFirst);
+        console.log(langueageIdSecond);
     }
 
     var compare = function (d) {
@@ -145,4 +153,91 @@ let drawBubbleChart = (dataset) => {
         });
 
 
+}
+
+//--------------------------.......................................
+
+let draw = (langueageIdFirst, langueageIdSecond) => {
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 20, left: 50 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#comparison")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+        data = [
+            {
+                group: "Most loved",
+                "c#": 12,
+                "java": 4
+            },
+            {
+                group: "Most dreaded",
+                "java": 14,
+                "c#": 6
+            }
+        ]
+
+        // List of subgroups = header of the csv files = soil condition here
+        //var subgroups = data.columns.slice(1)
+        console.log(subgroups)
+        var subgroups = ["c#", "java"]
+
+        // List of groups = species here = value of the first column called group -> I show them on the X axis
+        var groups = d3.map(data, function (d) { return (d.group) }).keys()
+        console.log(groups)
+        groups = ["Most loved", "Most dreaded"]
+
+        // Add X axis
+        var x = d3.scaleBand()
+            .domain(groups)
+            .range([0, width])
+            .padding([0.2])
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickSize(0));
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, 40])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Another scale for subgroup position?
+        var xSubgroup = d3.scaleBand()
+            .domain(subgroups)
+            .range([0, x.bandwidth()])
+            .padding([0.05])
+
+        // color palette = one color per subgroup
+        var color = d3.scaleOrdinal()
+            .domain(subgroups)
+            .range(d3.schemeSet2)
+
+        // Show the bars
+        svg.append("g")
+            .selectAll("g")
+            // Enter in data = loop group per group
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", function (d) { return "translate(" + x(d.group) + ",0)"; })
+            .selectAll("rect")
+            .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
+            .enter().append("rect")
+            .attr("x", function (d) { return xSubgroup(d.key); })
+            .attr("y", function (d) { return y(d.value); })
+            .attr("width", xSubgroup.bandwidth())
+            .attr("height", function (d) { return height - y(d.value); })
+            .attr("fill", function (d) { return color(d.key); });
+
+    
 }
